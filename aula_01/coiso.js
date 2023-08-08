24c42fd50646d75195c4434d95b1b557cd089429
João Vitor <joaomezzalira302@gmail.com>
2023-08-08 17:12:04 -0300

    coiso js

---
+++ aula_01/coiso.js
@@ -0,0 +1,60 @@
+ /*
+ const lista = ["Douglas", "Laízia", "Otávio", "Diego"];    
+ const tamanhoLista = lista.length -1;
+ console.log("Tamanho da lista " + tamanhoLista);
+
+ for(var i = 0; i <= tamanhoLista; i++){
+console.log("Aluno(a): " + lista[i]);
+ }
+
+ console.log("--------While--------")
+
+ var i2 = 0;
+ while(i2 <= tamanhoLista){
+console.log("Aliuno(a) " + lista[i2]);
+i2++;
+ }
+
+ lista.forEach((value, coiso)=>{
+    console.log(coiso + " - " + value);
+ });
+*/
+
+ //switch
+/*
+ const valorDeEntrada = 1;
+
+ switch (valorDeEntrada) {
+    case 1:
+        console.log("o valor é um");
+        break;
+ case 2: 
+ console.log("o valor é dos");
+        break;
+        case 3:
+            console.log("o value é three");
+            break;
+            default: console.log("valor undefined");
+ }
+*/
+ 
+
+ //object
+/*
+const alunos = {nome: "João", idade: 16};
+console.log(typeof alunos);
+
+atributo da variavel
+console.log(alunos.idade);
+*/
+
+/*
+const alunos = [{nome: "João", idade: 16}, {nome: "Douglas", idade: 16}, {nome: "Laízia", idade: 17}, {nome: "Otávio", idade: 16}];
+alunos.forEach((value) => {
+    if(typeof value == "object"){
+    console.log(value.nome, value.idade);
+}
+else{console.log(value);}
+});
+*/
+

c6ce732936cefef12ebe9efb403635e0ee78de04
João Vitor <joaomezzalira302@gmail.com>
2023-08-10 17:26:47 -0300

    toma ai a calculator

--- ex1.js
+++ ex1.js
@@ -11,32 +11,33 @@ resultado = num1 * num2;
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
-  });
-  readline.question(`Digite um número`, num1 => {
-    readline.question(`Digite outro número`, num2 => {
+});
+readline.question(`Digite um número `, num1 => {
     
-  readline.question(`Digite a op`, op => {
-    if(op == '+'){
-   resultado = num1 + num2 
-}
+    readline.question(`Digite outro número `, num2 => {
 
-if (op == '-'){
-resultado = num1 - num2
-}
-if (op == '*'){
-    resultado = num1 * num2
-    }
-    
-    if (op == '/'){
-        resultado = num1 / num2
-        }  
-        console.log(`o resultado é ${resultado}!`);
-        readline.close();
+        readline.question(`Digite a op `, op => {
+            console.log(typeof num1, typeof num2);
+            if (op == '+') {
+                resultado = Number(num1) + Number(num2)
+            }
+
+            if (op == '-') {
+                resultado =  Number(num1) - Number(num2)
+            }
+            if (op == '*') {
+                resultado =  Number(num1) * Number(num2)
+            }
+
+            if (op == '/') {
+                resultado =  Number(num1) / Number(num2)
+            }
+            console.log(`o resultado é ${resultado}!`);
+            readline.close();
+        });
+
+    });
 });
 
-});
-  });
-  
-  
 
-  
+
